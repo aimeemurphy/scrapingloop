4cdf69b63f2b15e06926b719d44d42c956064c31
aimee <asjmurphy@gmail.com>
2015-12-17 11:21:45 +0000

    working

---
+++ school2/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 15/12/2015
+# items.py file for school2 scraping project 
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+
+import scrapy
+
+from scrapy.item import Item, Field
+
+
+class School2Item(scrapy.Item):
+	sometext = scrapy.Field()
+	pagetitle = scrapy.Field()
+	url = scrapy.Field()
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+	pass
+
---
+++ school2/spiders/school_spider.py
@@ -0,0 +1,32 @@
+# making a spider to crawl
+
+# import the required packages and our item
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from school2.items import School2Item
+from scrapy.conf import settings
+
+
+# start with hard coding in a school, can functionalise later to get school info from a list
+class SchoolSpider(CrawlSpider): # based on inbuilt CrawlSpider class
+# tells spider how deep (think: 1.com/2/3) into website to go, see documentation for more on overriding scrapy middleware settings
+    #def __init__(self):
+     #   settings.overrides['DEPTH_LIMIT'] = 3
+    name = "kngs" # give the instance of the class a name - would be the name/urn of each school, this is what we use to intiate the crawl from terminal
+    allowed_domains = ["kngs.co.uk"] # restrict to crawling on pages beginning with the schools URL, can also use setting to do this...
+    start_urls = [
+    "http://www.kngs.co.uk"
+    ] # url to start crawling from
+    rules = [Rule(LinkExtractor(allow=['/+']), 'parse_page')] # only crawl urls which are the allowed domain + this, here it's just "/" so anything goes
+# the response object from this, the urls, is passed to the "parse_page" method below
+ 
+    def parse_page(self, response):
+        page = School2Item()
+        page['url'] = response.url
+        page['pagetitle'] = response.xpath("//title/text()").extract() # title nodes?
+        #page['sometext'] = response.css("//a[contains(text(),'school')]").extract()
+        #print # garbage, just chekcing something is happening
+        return page 
+        # tried return page['ur'l] <-- it doesn't work!!
+
+
