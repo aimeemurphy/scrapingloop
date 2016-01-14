b8862bc33ee25f2997e8f3402c20552a886faef3
aimee <asjmurphy@gmail.com>
2016-01-14 16:13:41 +0000

    website and spider name turned to variables

--- school2/spiders/school_spider.py
+++ school2/spiders/school_spider.py
@@ -10,15 +10,26 @@ import re
 import time
 
 
+# experimenting with making the school and url functions
+# name the spider - update to urn later
+a = "kngs1"
+# declare the website
+b = "http://www.kngs.co.uk"
+# from the website, construct the allowed domains *** need to consider whether is always going to be the right way to do it
+c = re.search("www",b)
+print c
+d = "kngs.co.uk"
+
+
 # start with hard coding in a school, can functionalise later to get school info from a list
 class SchoolSpider(CrawlSpider): # based on inbuilt CrawlSpider class
 # tells spider how deep (think: 1.com/2/3) into website to go, see documentation for more on overriding scrapy middleware settings
     #def __init__(self):
-    settings.overrides['DEPTH_LIMIT'] = 2
-    name = "kngs" # give the instance of the class a name - would be the name/urn of each school, this is what we use to intiate the crawl from terminal
-    allowed_domains = ["kngs.co.uk"] # restrict to crawling on pages beginning with the schools URL, can also use setting to do this...
+    settings.overrides['DEPTH_LIMIT'] = 1 # depth limit works
+    name = a # give the instance of the class a name - would be the name/urn of each school, this is what we use to intiate the crawl from terminal
+    allowed_domains = [d] # restrict to crawling on pages beginning with the schools URL, can also use setting to do this...
     start_urls = [
-    "http://www.kngs.co.uk"
+    b
     ] # url to start crawling from
     rules = [Rule(LinkExtractor(allow=['/+']), 'parse_page', follow=True)] 
     # only crawl urls which are the allowed domain + this, here it's just "/" so anything goes
@@ -32,8 +43,8 @@ class SchoolSpider(CrawlSpider): # based on inbuilt CrawlSpider class
         #page['soup'] = BeautifulSoup(response.body)
         #page['sometext'] = response.css("//a[contains(text(),'school')]").extract()
         #print # garbage, just chekcing something is happening
-        time.sleep(5) #
+        time.sleep(0.1) #pauses between successful scrapes (not errors or ignores), to stop website being overloaded
         return page 
         # tried return page['ur'l] <-- it doesn't work!!
 
-
+print c
